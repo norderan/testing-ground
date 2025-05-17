4750060799b02678405680cb1de24ce37c63b006
norderan <queider@proton.me>
2025-05-17 19:36:27 +0300

    Add basic image carusela

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./style.css";
 
 
+
+// Dropdown menu
 const html = {
     dropdownButton: document.getElementById("dropdown-button"),
     dropdownMenu: document.getElementById("dropdown-div"),
@@ -11,3 +13,47 @@ html.dropdownButton.addEventListener("click", () => {
     html.dropdownMenu.classList.toggle("not-visible");
 });
 
+
+// Image carusela
+
+const caruselaModule = (() => {
+    
+    const images = [
+        document.getElementById("div-image-1"),
+        document.getElementById("div-image-2"),
+        document.getElementById("div-image-3"),
+        document.getElementById("div-image-4"),
+    ]
+
+    const caruselaButtonLeft = document.getElementById("carusela-button-left");
+    const caruselaButtonRight = document.getElementById("carusela-button-right");
+
+    let currentImageIndex = 0;
+
+    function showImage(index) {
+        images.forEach((image, i) => {
+            image.classList.toggle("not-visible", i !== index);
+        });
+    }
+
+    function advanceImage() {
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+        showImage(currentImageIndex);
+    }
+
+    function retreatImage() {
+        currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+        showImage(currentImageIndex);
+    }
+
+    caruselaButtonLeft.addEventListener("click", retreatImage);
+    caruselaButtonRight.addEventListener("click", advanceImage);
+
+    return { advanceImage, retreatImage };
+})();
+
+
+
+setInterval(() => {
+    caruselaModule.advanceImage();
+}, 5000);
